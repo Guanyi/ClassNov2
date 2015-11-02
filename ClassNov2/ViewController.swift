9c20874fd4732cfb8d458589e69245d3773090b1
Guanyi Fang <a00075340@dhcp-142-232-233-145.wifi.bcit.ca>
2015-11-02 15:18:23 -0800

    finish step 5

--- ClassNov2/ViewController.swift
+++ ClassNov2/ViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate{
 
+    @IBOutlet weak var urlTextField: UITextField!
+    
+    @IBOutlet weak var webView: UIWebView!
+    @IBAction func goButtonPressed(sender: UIButton) {
+        
+        let text = urlTextField.text
+        let url = NSURL(string: text!)
+        print(text)
+        let request = NSURLRequest(URL: url!)
+        self.webView.loadRequest(request)
+        
+        webView.delegate = self
+        //resignFirstResponder()
+        
+        //hide keyboard when go pressed
+        urlTextField.endEditing(true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +40,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func webView( webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        print("should start load")
+        return true
+    }
+    
+    func webViewDidStartLoad( webView: UIWebView) {
+        print("did start load")
+    }
+    
+    func webViewDidFinishLoad( webView: UIWebView) {
+        print("did finish load")
+    }
+    
+    func webView( webView: UIWebView, didFailLoadWithError error: NSError?) {
+        print(error)
+    }
 
 }
-
