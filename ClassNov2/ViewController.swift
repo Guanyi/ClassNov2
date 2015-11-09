76160f1dd94a3ab444b0a4f41123b6b6427c9d84
Guanyi Fang <guanyi@mini1.local>
2015-11-08 20:55:59 -0800

    done

--- ClassNov2/ViewController.swift
+++ ClassNov2/ViewController.swift
@@ -12,17 +12,21 @@ import WebKit
 class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate{
 
     @IBOutlet weak var urlTextField: UITextField!
-    
     @IBOutlet weak var webView: UIWebView!
-    @IBAction func goButtonPressed(sender: UIButton) {
+    
+    @IBAction func localButtonPressed(sender: UIButton) {
+        let url = NSBundle.mainBundle().URLForResource("MyTest", withExtension:"html")
+        let request = NSURLRequest(URL: url!)
+        self.webView.loadRequest(request)
+        //hide keyboard when go pressed
+        urlTextField.endEditing(true)
+    }
+    @IBAction func goButtonPressed(sender: AnyObject) {
         
         let text = urlTextField.text
         let url = NSURL(string: text!)
-        print(text)
         let request = NSURLRequest(URL: url!)
         self.webView.loadRequest(request)
-        
-        webView.delegate = self
         //resignFirstResponder()
         
         //hide keyboard when go pressed
@@ -33,16 +37,33 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
+        urlTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        goButtonPressed(textField)
+        return false
+    }
 
     
     func webView( webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         print("should start load")
+        print(request)
+        if request.URL?.scheme == "local" {
+            webView.stringByEvaluatingJavaScriptFromString("showAlert()")
+            return false
+        }
+        
+        if request.URL?.scheme == "script" {
+            goToApple()
+            //return false
+        }
         return true
     }
     
@@ -56,6 +77,16 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate{
     
     func webView( webView: UIWebView, didFailLoadWithError error: NSError?) {
         print(error)
+    }
+    
+    
+    func goToApple() {
+        let appleAddress: String = "http://www.apple.com"
+        let request = NSURLRequest(URL: NSURL(string: appleAddress)!)
+        self.webView.loadRequest(request)
+        urlTextField.text = appleAddress
+        //hide keyboard when go pressed
+        urlTextField.endEditing(true)
     }
 
 }
